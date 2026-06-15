6133748732ae56460985cdc56467ba1c4c1fe628
hamzacyberpatcher <hamzaoffuture@gmail.com>
2026-06-15 20:18:11 +0500

    fixed a prod error

--- app/components/AboutUs.tsx
+++ app/components/AboutUs.tsx
@@ -7,7 +7,6 @@ const AboutUs = () => {
     <div className="w-full flex flex-col lg:flex-row bg-brand-primary overflow-hidden py-6 sm:py-8 lg:py-0 h-[calc(100vh-var(--spacing-navbar))]">
       
       <ScrollReveal delay={0.2} direction="up" 
-        direction="right" 
         className="w-full lg:w-1/2 flex items-center justify-center px-5 sm:px-8 lg:px-0 pt-4 lg:pt-0 h-1/2 lg:h-full"
       >
         <div className="relative w-[60%] sm:w-[50%] md:w-[45%] lg:w-[65%] xl:w-[55%] h-full max-h-[90%]">
@@ -24,8 +23,6 @@ const AboutUs = () => {
       </ScrollReveal>
 
       <ScrollReveal delay={0.2} direction="down" 
-        direction="left" 
-        delay={0.1} 
         className="w-full lg:w-1/2 px-5 sm:px-8 lg:px-12 xl:px-16 flex items-center py-4 lg:py-10"
       >
         <div className="w-full space-y-3 sm:space-y-4">
