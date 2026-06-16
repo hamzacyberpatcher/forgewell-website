8f392e57fc7a9599fa015fadd62e7d140f21fa9c
hamzacyberpatcher <hamzaoffuture@gmail.com>
2026-06-16 17:12:53 +0500

    fixed the overflow on x axis

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,11 +1,11 @@
 import type { Metadata } from 'next'
-import { Inter, Geist } from 'next/font/google'
+import { Inter } from 'next/font/google'
 import './globals.css'
 import Navbar from './components/Navbar'
 import { cn } from "@/lib/utils";
 import Footer from './components/Footer';
 
-const geist = Geist({subsets:['latin'],variable:'--font-sans'})
+const inter = Inter({subsets:['latin'],variable:'--font-sans'})
 
 export const metadata: Metadata = {
   title: 'Forgewell Precision | Component Manufacturing',
@@ -18,7 +18,7 @@ export default function RootLayout({
   children: React.ReactNode
 }) {
   return (
-    <html lang="en" className={cn("font-sans", geist.variable)}>
+    <html lang="en" className={cn("font-sans", inter.variable)}>
       
       <body>
         <Navbar />
--- next.config.ts
+++ next.config.ts
@@ -4,4 +4,8 @@ const nextConfig: NextConfig = {
   /* config options here */
 };
 
+module.exports = {
+  allowedDevOrigins: ['192.168.100.49'],
+}
+
 export default nextConfig;
